43e72d394656103e28cfac1ddf9bbc419f01028c
Giorgio Desideri <giorgio.desideri@gmail.com>
2025-11-30 15:26:29 +0700

    fix: errors and warnings

--- src/DotNetConfTh2025.Api/Services/ProductWriteService.cs
+++ src/DotNetConfTh2025.Api/Services/ProductWriteService.cs
@@ -4,10 +4,14 @@ namespace DotNetConfTh2025.Api.Services;
 
 public class ProductWriteService : IProductWriteService
 {
-    public Task<NewProductItem?> SaveNewProductAsync(SaveProductRequest request, CancellationToken token)
+    public Task<NewProductItem?> SaveNewProductAsync(
+        SaveProductRequest request,
+        CancellationToken token
+    )
     {
         // persist the request values inside a new product entity
-
-        return new NewProductItem(123123L, "New Product", 90);
+#pragma warning disable CS8619 // Nullability of reference types in value doesn't match target type.
+        return Task.FromResult(new NewProductItem(123123L, "New Product", 90));
+#pragma warning restore CS8619 // Nullability of reference types in value doesn't match target type.
     }
 }
